6c4c882b4fa99422e5a5ea1f40ed1044dec6a60d
David Kincaid <daelonsuzuka@gmail.com>
2022-04-27 18:03:23 -0400

    Started working on scene-analyzer

--- src/extension.ts
+++ src/extension.ts
@@ -1,18 +1,23 @@
 import { ExtensionContext } from "vscode";
 import { GodotTools } from "./godot-tools";
+import { GodotAnalyzer } from "./analyzer/analyzer";
 import debuggerContext = require("./debugger/debugger_context");
 
 let tools: GodotTools = null;
+let analyzer: GodotAnalyzer = null;
 
 export function activate(context: ExtensionContext) {
 	tools = new GodotTools(context);
 	tools.activate();
 	debuggerContext.register_debugger(context);
+	analyzer = new GodotAnalyzer(context);
+	analyzer.activate();
 }
 
 export function deactivate(): Thenable<void> {
 	return new Promise<void>((resolve, reject) => {
 		tools.deactivate();
+        analyzer.deactivate();
 		resolve();
 	});
 }
